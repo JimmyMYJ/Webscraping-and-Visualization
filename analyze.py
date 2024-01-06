ee162beedda0f3a2bd3433c65ce50c0ed379fb0b
hpeng04 <harry.h.peng@gmail.com>
2024-01-06 15:46:33 -0700

    updated code for api

--- analyze.py
+++ analyze.py
@@ -1,11 +1,15 @@
-import openai
-openai.api_key = open("key.txt", "r").read().strip('\n')
-output = openai.ChatCompletion.create(
-    model = "gpt-3.5-turbo"
-    messages = [
-        {
-            "role": "user",
-            "content": #Input Data
-        }
-    ]
-)
+from openai import OpenAI
+client = OpenAI(api_key=(open("key.txt", "r").read()))
+
+def get_response(input_data):
+    response = client.chat.completions.create(
+    model="gpt-3.5-turbo-1106",
+    messages=[
+        {"role": "user", 
+        "content": input_data}
+    ],
+    max_tokens=150,
+    )
+    return response
+
+print(get_response("hi"))
