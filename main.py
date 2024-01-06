b4232be5175acce04fd096673091ebaf712eeee8
Ethan Shi <ethanshi322@gmail.com>
2024-01-06 13:39:42 -0700

    find jobs

--- main.py
+++ main.py
@@ -2,10 +2,34 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+service = Service("chromedriver.exe")
+driver = webdriver.Chrome(service=service)
+driver.get("https://ca.indeed.com/")
+
+searchJob = driver.find_element(By.ID,"text-input-what")
+searchJob.clear()
+searchJob.send_keys("Programming")
+
+searchArea = driver.find_element(By.ID,"text-input-where")
+searchArea.clear()
+searchArea.send_keys("Edmonton, AB" + Keys.ENTER)
+
+jobTitle = []
+maximumPages = 5
+#for i in range(maximumPages):
+time.sleep(3)
+
+jobListings = driver.find_element(By.ID,"mosaic-provider-jobcards")
+jobs = jobListings.find_elements(By.CLASS_NAME,"job_seen_beacon")
 
 
-from  selenium import  webdriver
 
-website = "https://ca.indeed.com/"
-path = "chromedriver-win64/chromedriver.exe"
-driver = webdriver.Chrome(path)
+
+time.sleep(10)
+driver.quit()
