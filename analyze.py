8f0fab854204c057cb265d9e9c5d84bb7cd30f45
JimmyMYJ <144820759+JimmyMYJ@users.noreply.github.com>
2024-01-07 00:54:16 -0700

    analyze.py

--- analyze.py
+++ analyze.py
@@ -1,17 +1,20 @@
 from openai import OpenAI
 client = OpenAI(api_key=(open("key.txt", "r").read()))
 input_data = open("result.txt", "r").read()
-def get_response(input_data):
+resume = open("resume.txt","r").read()
+def get_response(input_data,resume):
     response = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
         {
             "role": "system",
-            "content": "Compare the job posting and resume and provide improvements to the resume.", #Need another role for file upload
+            "content": "Compare the job posting and a resume and provide improvements to the resume.",#Need another role for file upload
+           
         },
         {
             "role": "user", 
-            "content": input_data
+            "content": input_data and resume
+            
         }
     ],
     max_tokens=150,
